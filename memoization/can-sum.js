5b0cc566d72404f476f4af35b173b0bbebe5ccc1
HADDAD Moez <mhaddad@ats-digital.com>
2021-02-19 23:36:22 +0100

    best sum

--- memoization/can-sum.js
+++ memoization/can-sum.js
@@ -1,9 +1,9 @@
-// Write a function `canSum(targetSum, numbers)` that takes in a 
+// Write a function `canSum(targetSum, numbers)` that takes in a
 // targetSum and numbers as arguments
-// The function should return a boolean indicating whether or not 
+// The function should return a boolean indicating whether or not
 // it is possible to generate the targetSum using numbers from the array
 // You may use an element of the array as many times as needed
-// you may assume that all input numbers are > 0 
+// you may assume that all input numbers are > 0
 
 const canSum = (targetSum, numbers, memo = {}) => {
   if (targetSum in memo) return memo[targetSum];
